181e99e52024ae887567dd04d926e886c2d4a0c0
Jacob Maristany <jacob-maristany@users.noreply.github.com>
2021-11-23 15:43:37 -0600

    Fixed rm command in build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 msbuild -t:Clean,Build -p:Configuration=Release ./Naxam.Mapbox.iOS/Naxam.Mapbox.iOS.csproj
 echo moving files
-rm ./output
+rm -r ./output
 mv -f ./Naxam.Mapbox.iOS/bin/Release/ ./output
