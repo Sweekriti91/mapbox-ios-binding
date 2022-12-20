c26a802e14260d04a3ccbf1c9cbf25b076a5bd91
Sweekriti Satpathy <swsat@microsoft.com>
2022-12-20 11:31:50 -0800

    common builds

--- Naxam.Mapbox.Common/Properties/AssemblyInfo.cs
+++ Naxam.Mapbox.Common/Properties/AssemblyInfo.cs
@@ -1,26 +1,26 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿//using System.Reflection;
+//using System.Runtime.CompilerServices;
 
-// Information about this assembly is defined by the following attributes. 
-// Change them to the values specific to your project.
+//// Information about this assembly is defined by the following attributes. 
+//// Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Naxam.Mapbox.Common")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("(c) Ha Do")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+//[assembly: AssemblyTitle("Naxam.Mapbox.Common")]
+//[assembly: AssemblyDescription("")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("")]
+//[assembly: AssemblyProduct("")]
+//[assembly: AssemblyCopyright("(c) Ha Do")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
 
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+//// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+//// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+//// and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("4.10.0")]
+//[assembly: AssemblyVersion("4.10.0")]
 
-// The following attributes are used to specify the signing key for the assembly, 
-// if desired. See the Mono documentation for more information about signing.
+//// The following attributes are used to specify the signing key for the assembly, 
+//// if desired. See the Mono documentation for more information about signing.
 
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
+////[assembly: AssemblyDelaySign(false)]
+////[assembly: AssemblyKeyFile("")]
