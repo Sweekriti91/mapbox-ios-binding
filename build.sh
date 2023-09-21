1f70e1272dece0c6cdfcbe5a34b4ee1e8b3a957c
Sweekriti Satpathy <swsat@microsoft.com>
2023-09-21 12:28:05 -0700

    dotnetify build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-msbuild -t:Clean,Build -p:Configuration=Release ./Naxam.Mapbox.iOS/Naxam.Mapbox.iOS.csproj
+dotnet build -t:Clean,Build -p:Configuration=Release ./Naxam.Mapbox.iOS/Naxam.Mapbox.iOS.csproj
 echo moving files
 rm -r ./output
-mv -f ./Naxam.Mapbox.iOS/bin/Release/ ./output
+mv -f ./Naxam.Mapbox.iOS/bin/Release/net7.0-ios/ ./output
